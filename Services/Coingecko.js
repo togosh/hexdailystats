a426ce78cfe19cd6541e6553ba0798f04ae7df6c
Codeak <jfivec1@live.spcollege.edu>
2021-11-17 07:10:34 -0500

    coingecko fixes

--- Services/Coingecko.js
+++ Services/Coingecko.js
@@ -1,7 +1,40 @@
 
 ///////////////////////////////////////////////////
 // COINGECKO PRICES
-
+const FETCH_SIZE = 1048576;
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+var fetchRetry = require('fetch-retry')(fetch, { 
+    retryOn: async function(attempt, error, response) {
+      if (attempt > 3) { return false; }
+  
+      if (error !== null) {
+        log(`FETCH --- RETRY ${attempt + 1} --- ERROR --- ` + error.toString()); await sleep(500);
+        return true;
+    } 
+  
+      if (response.status >= 400) {
+        log(`FETCH --- RETRY ${attempt + 1} --- STATUS --- ` + response.status); await sleep(500);
+        return true;
+      }
+  
+      try {
+        var response2 = await response.clone().buffer();
+        const json = JSON.parse(response2);
+  
+        if (json.errors && Object.keys(json.errors).length > 0) {
+            if (json.errors[0].message) {
+              log(`FETCH --- INTERNAL JSON ERROR --- ${attempt + 1} --- ` + json.errors[0].message); await sleep(500);
+              return true;
+            }
+        }
+        
+        return false;
+      } catch (error) {
+        log(`FETCH --- RETRY ${attempt + 1} --- JSON ---` + error.toString()); await sleep(500);
+        return true;
+      }
+    }
+  });
 async function getPriceAllTimeHigh(){
     var url = "https://api.coingecko.com/api/v3/coins/hex";
     return await fetchRetry(url, {
@@ -16,7 +49,7 @@ async function getPriceAllTimeHigh(){
     });
   }
   
-  async function getPriceHistory_Bitcoin(){
+  async function getPriceHistory_Bitcoin(currentDayGlobal){
     var url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=" + currentDayGlobal + "&interval=daily";
     return await fetchRetry(url, {
       method: 'GET',
@@ -30,7 +63,7 @@ async function getPriceAllTimeHigh(){
     });
   }
   
-  async function getPriceHistory_Ethereum(){
+  async function getPriceHistory_Ethereum(currentDayGlobal){
     var url = "https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=" + currentDayGlobal + "&interval=daily";
     return await fetchRetry(url, {
       method: 'GET',
@@ -44,7 +77,7 @@ async function getPriceAllTimeHigh(){
     });
   }
   
-  async function getPriceHistory_Gold(){
+  async function getPriceHistory_Gold(currentDayGlobal){
     var url = "https://api.coingecko.com/api/v3/coins/pax-gold/market_chart?vs_currency=usd&days=" + currentDayGlobal + "&interval=daily";
     return await fetchRetry(url, {
       method: 'GET',
@@ -62,13 +95,13 @@ async function getPriceAllTimeHigh(){
     getPriceAllTimeHigh: async () => {
        return await getPriceAllTimeHigh();
     }
-    ,getPriceHistory_Bitcoin: async () => {
-        return await getPriceHistory_Bitcoin();
+    ,getPriceHistory_Bitcoin: async (currentDayGlobal) => {
+        return await getPriceHistory_Bitcoin(currentDayGlobal);
     }
-    ,getPriceHistory_Ethereum: async () => {
-        return await getPriceHistory_Ethereum();
+    ,getPriceHistory_Ethereum: async (currentDayGlobal) => {
+        return await getPriceHistory_Ethereum(currentDayGlobal);
     }
-    ,getPriceHistory_Gold: async () => {
-        return await getPriceHistory_Gold();
+    ,getPriceHistory_Gold: async (currentDayGlobal) => {
+        return await getPriceHistory_Gold(currentDayGlobal);
     }
  }

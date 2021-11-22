e97989c52a9b824af253dae313930e8d8204fa22
Togo <togoshigekata@gmail.com>
2021-11-21 18:42:14 -0600

    - add missing number formatting function to Twitter service

--- Services/Twitter.js
+++ Services/Twitter.js
@@ -154,6 +154,32 @@ function log(message){
   console.log(new Date().toISOString() + ", " + message);
 }
 
+function nFormatter(num, digits) {
+  try {
+      const lookup = [
+          { value: 1, symbol: "" },
+          { value: 1e3, symbol: "k" },
+          { value: 1e6, symbol: "M" },
+          { value: 1e9, symbol: "B" },
+          { value: 1e12, symbol: "T" }
+      ];
+      const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+      var item = lookup.slice().reverse().find(function(item) {
+          return num >= item.value;
+      });
+
+      return { 
+          amount: (num / item.value).toFixed(digits).replace(rx, "$1"),
+          symbol: item.symbol
+      }
+  } catch {
+      return {
+          amount: 0,
+          symbol: ''
+      };
+  }
+}
+
 module.exports = { 
     tweet: async (dailyStat) => {
        return await tweet(dailyStat);
